7c63bc8177c58cef205ebf73af26f24fd933a559
byBlurr <dbldumont11@gmail.com>
2021-02-02 16:39:26 +0000

    Added CalculateHash() and VerifyHash()

---
+++ ThatBlurrLibrary/Encryption/Hash.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blurr.Encryption
+{
+    public class Hash
+    {
+        private Hash() {  }
+
+        /// <summary>
+        /// Calculate hash from string
+        /// </summary>
+        /// <param name="input">The string to calculate hash from</param>
+        /// <returns>The hash string</returns>
+        public static string CalculateHash(string input, HashType hashType)
+        {
+            HashAlgorithm hashAlgorithm;
+            switch (hashType)
+            {
+                case HashType.MD5:
+                    hashAlgorithm = MD5.Create();
+                    break;
+                case HashType.SHA1:
+                    hashAlgorithm = SHA1.Create();
+                    break;
+                case HashType.SHA256:
+                    hashAlgorithm = SHA256.Create();
+                    break;
+                case HashType.SHA384:
+                    hashAlgorithm = SHA384.Create();
+                    break;
+                case HashType.SHA512:
+                    hashAlgorithm = SHA512.Create();
+                    break;
+                default:
+                    hashAlgorithm = MD5.Create();
+                    break;
+            }
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = hashAlgorithm.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (byte t in hash)
+            {
+                sb.Append(t.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Verify that the hash matches
+        /// </summary>
+        /// <param name="input">The string to compare</param>
+        /// <param name="hash">The hash to compare</param>
+        /// <param name="hashType">The algorithm used for hash</param>
+        /// <returns>Returns true if the two hashes match</returns>
+        public static bool VerifyHash(string input, string hash, HashType hashType)
+        {
+            string testHash = CalculateHash(input, hashType);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return comparer.Compare(testHash, hash) == 0;
+        }
+    }
+
+    public enum HashType
+    {
+        MD5, SHA1, SHA256, SHA384, SHA512
+    }
+}
---
+++ UnitTest/ApiTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Tests for API methods
+    /// </summary>
+    [TestClass]
+    public class ApiTests
+    {
+        /// <summary>
+        /// Test making a call
+        /// </summary>
+        [TestMethod]
+        public void ApiCallTest()
+        {
+            // TODO: well I dont have an API to test this on rn...
+        }
+    }
+}
