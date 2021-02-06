a03d805dc553dd4dc323064894aac670421239c4
byBlurr <dbldumont11@gmail.com>
2021-02-06 12:37:38 +0000

    Added Util.Log()

---
+++ ThatBlurrLibrary/Util.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Blurr
+{
+    public class Util
+    {
+        private Util() { }
+
+        /// <summary>
+        /// Create a console log
+        /// </summary>
+        /// <param name="logType">Type of console message</param>
+        /// <param name="source">The source of the message</param>
+        /// <param name="message">The message</param>
+        public static void Log(LogType logType, string source, string message)
+        {
+            DateTime time = DateTime.Now;
+
+            switch (logType)
+            {
+                case LogType.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogType.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogType.Info:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case LogType.Verbose:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case LogType.Important:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+            }
+
+            Console.WriteLine($"{time,20} - [{logType,8}] {source,10}: {message}");
+            Console.ResetColor();
+        }
+    }
+
+    /// <summary>
+    /// The different log types
+    /// </summary>
+    public enum LogType
+    {
+        Warning, Error, Info, Verbose, Important
+    }
+}
